cabcf77f484eed31f200792505b164ed0017268f
casey-minzi <cluvhope@gmail.com>
2020-05-04 12:03:02 +0100

    Change Colours

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
 
 canvas.width = 600;
 canvas.height=500;
@@ -28,9 +29,16 @@ function onMouseMove(e) {
     } 
 }
 
+function handleColorClick(e) {
+    const colorPicked = e.target.style.backgroundColor
+    ctx.strokeStyle = colorPicked;
+}
+
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
