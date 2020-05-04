4219bf50aa0952ab2c0771ecc82936ff8e85b0f8
casey-minzi <cluvhope@gmail.com>
2020-05-04 13:50:36 +0100

    Save Image by Button

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
 const modeBtn = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
 
 const INITIAL_COLOUR = '#2c2c2c';
 
 canvas.width = 600;
 canvas.height=500;
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.lineWidth = 2.5;
 ctx.strokeStyle = INITIAL_COLOUR;
 ctx.fillStyle = INITIAL_COLOUR;
@@ -62,12 +65,25 @@ function handleCanvasClick() {
     }
 }
 
+function handleCM(e) {
+    e.preventDefault();
+}
+
+function handleSaveBtn(e) {
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'DoodlePad';
+    link.click();
+}
+
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
     canvas.addEventListener('click', handleCanvasClick);
+    canvas.addEventListener('contextmenu', handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
@@ -78,4 +94,8 @@ if (range) {
 
 if (modeBtn) {
     modeBtn.addEventListener('click', handleModeBtnClick);
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener('click', handleSaveBtn);
 }
